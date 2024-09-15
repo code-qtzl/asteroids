5a4c9003d201a131f98b21a2ee1f47e303d95c83
code-qtzl <azepeda90@outlook.com>
2024-09-14 22:29:03 -0500

    creating groups, updating player

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import sys
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}\nStarting asteroids!')
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
