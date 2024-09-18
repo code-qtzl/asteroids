fc0590edc5d1928a350905206360bea0c0160da8
code-qtzl <azepeda90@outlook.com>
2024-09-17 19:39:44 -0500

    lmited the amount of shots at a time

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.limit_shots = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,6 +23,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.limit_shots -= dt
 
         if keys[pygame.K_LEFT]:
             self.rotate(-dt)
@@ -35,11 +37,15 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
 
+        
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.limit_shots > 0:
+            return False
         shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.limit_shots = PLAYER_SHOOT_COOLDOWN
